49ed821f84ed6cba89b5957784e36d6ed3a1b412
Juliane Cassidy <julianecassidy@gmail.com>
2023-05-31 17:49:19 -0600

    add binary-tree exercises

--- binary-tree.js
+++ binary-tree.js
@@ -14,18 +14,82 @@ class BinaryTreeNode {
    * a node with less than two children. */
   minDepthToIncompleteNode() {
 
+    // base case: if this.left or this.right is null, return 1
+    if (this.left === null || this.right === null) return 1;
+
+    let minDepth = 1;
+
+    let leftDepth = 0;
+    leftDepth += this.left.minDepthToIncompleteNode();
+
+    let rightDepth = 0;
+    rightDepth += this.right.minDepthToIncompleteNode();
+
+    return minDepth + Math.min(leftDepth, rightDepth);
+
+    // variable for min depth, initially 1
+
+    // have a variable to track left depth
+    // check left side
+        // left depth + this.left.minDepth()
+
+    // have a variable to track right depth
+    // check right side
+        // right depth + this.right.minDepth()
+        
+    // return min depth + the min of left depth/right depth
   }
 
   /** maxDepth(): return the maximum depth from the invoking node -- that is,
    * the length of the longest path from the invoking node to a leaf. */
   maxDepth() {
+    if (this.left === null && this.right === null) return 1;
 
+    let maxDepth = 1;
+    const leftDepth = this.left !== null ? this.left.maxDepth() : 0;
+    // let leftDepth = 0;
+    // if (this.left !== null) {
+    // leftDepth += this.left.maxDepth();
+    // }
+
+    const rightDepth = this.right !== null ? this.right.maxDepth() : 0;
+    // let rightDepth = 0;
+    // if (this.right !== null) {
+    // rightDepth += this.right.maxDepth();
+    // }
+
+    return maxDepth + Math.max(leftDepth, rightDepth);
   }
 
   /** minDepth(): return the minimum depth from the invoking node -- that is,
    * the length of the shortest path from the invoking node to a leaf. */
    minDepth() {
+     console.log("THIS", this);
+    if (this.left === null && this.right === null) return 1;
 
+    let minDepth = 1;
+
+    let leftDepth = 0;
+    if (this.left !== null) {
+    leftDepth += this.left.minDepth();
+    }
+
+    let rightDepth = 0;
+    if (this.right !== null) {
+    rightDepth += this.right.minDepth();
+    }
+
+    console.log("LEFT:", leftDepth, "RIGHT:", rightDepth);
+    if (leftDepth > 0 && rightDepth > 0) {
+      console.log("both are greater than 0");
+      return minDepth + Math.min(leftDepth, rightDepth);
+    } else if (leftDepth === 0) {
+      console.log("left depth is 0")
+      return minDepth + rightDepth;
+    } else {
+      console.log("right is 0");
+      return minDepth + leftDepth;
+    }
   }
 
   /** nextLarger(lowerBound): return the smallest value from the invoking node
@@ -48,7 +112,8 @@ class BinaryTree {
   // this is a stack or recursion problem; we'll use recursion
 
   minDepthToIncompleteNode() {
-
+    if(!this.root) return 0;
+    return this.root.minDepthToIncompleteNode();
   }
 
   /** maxDepth(): return the maximum depth of the tree -- that is,
@@ -57,7 +122,8 @@ class BinaryTree {
   // this is a stack or recursion problem; we'll use recursion
 
   maxDepth() {
-
+    if(!this.root) return 0;
+    return this.root.maxDepth();
   }
 
   /** minDepth(): return the minimum depth of the tree -- that is,
@@ -66,7 +132,8 @@ class BinaryTree {
   // this is a stack or recursion problem; we'll use recursion
 
   minDepth() {
-
+    if(!this.root) return 0;
+    return this.root.minDepth();
   }
 
   /** nextLarger(lowerBound): return the smallest value in the tree
